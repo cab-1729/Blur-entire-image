8f2ca2157b2c5f3d6561c693b0847af0b2cf1b91
codingjustahobby <60106638+codingjustahobby@users.noreply.github.com>
2020-02-19 09:37:49 +0530

    Add files via upload

---
+++ cartoonize.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import sys
+from itertools import repeat
+original=Image.open(sys.argv[1])
+new_image=Image.new('RGB',original.size)
+width,height=original.size
+da=sys.argv[3]
+if da.count('-')!=2:
+    degree=tuple(repeat(int(da)))
+else:
+    degree=tuple([int(i) for i in da.split('-')])
+def color_pix(pixes,color):
+    for i in pixes:
+        new_image.putpixel(i,color)
+def for_each_pixel(func):
+    for r in range(height):
+        for c in range(width):
+            address=(c,r)
+            color=original.getpixel(address)
+            func(address,color)
+def pix_avg(pixes):
+    rs,gs,bs=0,0,0
+    for x,y,z in pixes:
+        rs+=x
+        gs+=y
+        bs+=z
+    stuff=len(pixes)
+    return (rs//stuff,gs//stuff,bs//stuff)
+already_found=[]
+categories={}
+def sort_pixels(address,color):
+    global categories
+    key_val=str([i//j for i,j in zip(color,degree)])
+    if key_val in already_found:
+        category=categories[key_val]
+        category['positions'].append(address)
+        category['colors'].append(color)
+    else:
+        already_found.append(key_val)
+        categories[key_val]={'positions':[address],'colors':[color]}
+for_each_pixel(sort_pixels)
+for cutout in categories.values():
+    color_pix(cutout['positions'],pix_avg(cutout['colors']))
+new_image.save(sys.argv[2])
